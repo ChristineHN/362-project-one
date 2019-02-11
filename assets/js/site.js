e3986b0e53207746761ed3a5858588e42ba4a181
christineHN <hnoh3@hawk.iit.edu>
2019-02-10 21:24:33 -0600

    checked validator and linter

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,3 +1,6 @@
+document.addEventLister('DOMContentLoaded', function(){
+  document.getElementById("signup").addEventLister("click", msg);
+});
 
 function msg() {
   alert("Thank you for joining us for the beta test!");
